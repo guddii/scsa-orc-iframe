f0ca9fd19f0aa18ee8fe6f2b7169b17a644e2e05
Florian Gudat <florian.gudat@guices.com>
2019-09-25 13:16:39 +0200

    Enable account

--- client/src/scripts/App.ts
+++ client/src/scripts/App.ts
@@ -11,6 +11,7 @@ export class App implements MessagingEndpoints {
 
     constructor(messagingSystem: MessagingSystem) {
         this.channel = messagingSystem.channel;
+        this.channel.subscribe(this, Applications.ACCOUNT.name);
         this.channel.subscribe(this, Applications.CATALOGUE.name);
         this.channel.subscribe(this, Applications.CHECKOUT.name);
     }
@@ -22,12 +23,16 @@ export class App implements MessagingEndpoints {
     publish() {
         try {
             this.channel.publish(
-                new Message({ hello: "Checkout, are you there?" }),
-                Applications.CHECKOUT.name
+                new Message({ hello: "Account, are you there?" }),
+                Applications.ACCOUNT.name
             );
             this.channel.publish(
                 new Message({ hello: "Catalogue, are you there?" }),
                 Applications.CATALOGUE.name
+            );
+            this.channel.publish(
+                new Message({ hello: "Checkout, are you there?" }),
+                Applications.CHECKOUT.name
             );
         } catch (error) {
             console.warn(error);
